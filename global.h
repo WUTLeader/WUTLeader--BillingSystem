df557bfb7e366641beb3a4786ed458be466099ea
WUTLeader <igthedou@qq.com>
2026-09-23 16:22:05 +0800

    删除旧C项目代码，准备上传新版VS代码

--- global.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-//宏定义
-#define FALSE 0
-#define TRUE 1
-
-#endif // !GLOBAL_H
-
--- tool.h
+++
@@ -1,7 +0,0 @@
-#ifndef TOOL_H
-#define TOOL_H
-
-void timeToString(time_t t, char* pBuf);
-
-#endif // !TOOL_H
-
